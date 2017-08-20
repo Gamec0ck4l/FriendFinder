d770f2e54e3b2e4b2b64390c5822cfa869f3f466
Gamec0ck4l <theelongnguyen@gmail.com>
2017-08-20 17:17:39 -0400

    completed server.js & freinds.js

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+    {
+    "name": "Leonardo DiCaprio",
+    "photo": "http://cdn4.thr.com/sites/default/files/2014/11/leonardo_dicaprio.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Dwayne Johnson",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/f/f1/Dwayne_Johnson_2%2C_2013.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Denzel Washington",
+    "photo": "https://i.pinimg.com/originals/2f/72/41/2f72417d5d6a580ab37a4d925c9e3a8d.jpg",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Rick Yune",
+    "photo": "http://liverampup.com/uploads/images/celebrity/rick%20yune.jpg",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Jessica Alba",
+    "photo": "https://i.pinimg.com/736x/95/a5/55/95a5558c856dab05349e6c74b4327014--jessica-alba-hairstyles-short-hairstyles.jpg",
+    "scores": [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Charlize Theron",
+    "photo": "http://1.bp.blogspot.com/-YvWruOdpDGM/ULJuTHeQVBI/AAAAAAAAAIc/tgrpw-DZTq0/s1600/Charlize.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    },
+    {
+    "name": "Freida Pinto",
+    "photo": "https://i.pinimg.com/originals/f8/6b/c1/f86bc16fc19c502a06908f81cee114f0.jpg",
+    "scores": [
+    "2",
+    "4",
+    "2",
+    "4",
+    "2",
+    "2",
+    "5",
+    "4",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Halle Berry",
+    "photo": "http://cdn-img.instyle.com/sites/default/files/styles/276x276/public/images/2014/TRANSFORMATIONS/2007-2-halle-berry-567_0.jpg?itok=j8riTiL5",
+    "scores": [
+    "2",
+    "4",
+    "2",
+    "4",
+    "2",
+    "2",
+    "5",
+    "4",
+    "3",
+    "3"
+    ]
+    },
+    {
+    "name": "Ursula",
+    "photo": "https://vignette1.wikia.nocookie.net/disney/images/c/c5/Ursula_transparent.png/revision/latest?cb=20170426170016",
+    "scores": [
+    "3",
+    "3",
+    "2",
+    "4",
+    "2",
+    "3",
+    "2",
+    "4",
+    "1",
+    "3"
+    ]
+    },
+    {
+    "name": "Shrek",
+    "photo": "https://vignette4.wikia.nocookie.net/shrek/images/8/87/Shrek_fierce.jpg/revision/latest?cb=20100604223615",
+    "scores": [
+    "4",
+    "3",
+    "2",
+    "1",
+    "3",
+    "3",
+    "5",
+    "2",
+    "3",
+    "4"
+    ]
+    }
+    ];
+
+    module.exports = friendsArray;
